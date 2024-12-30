4404167781d19a556865ad58cfc37a0a5838b956
root <mtrhoades0727@gmail.com>
2024-12-30 12:26:02 -1000

    switched out some carousel images, moving header overlay down to bottom of carousel, fixed position

--- src/components/CarouselHero.js
+++ src/components/CarouselHero.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { MDBContainer, MDBCol, MDBRow, MDBBtn } from 'mdb-react-ui-kit';
 import img0136 from '../images/Asher garage images/drivewaygates/carouselimages/IMG_0136.jpg';
-import image0140 from '../images/Asher garage images/drivewaygates/carouselimages/IMG_0140.jpg';
-import image0293 from '../images/Asher garage images/drivewaygates/carouselimages/IMG_0293.jpg';
+import image9253 from '../images/Asher garage images/residential/carouselimages/IMG_9253.jpg';
+import image9120 from '../images/Asher garage images/residential/carouselimages/IMG_9120.jpg';
 import image0343 from '../images/Asher garage images/drivewaygates/carouselimages/IMG_0343.jpg';
 
 import AboutModal from './Modals/AboutModal';
@@ -14,42 +14,42 @@ const CarouselHero = () => {
 
   return (
     <div className="carouselContainer" id="hero">
-      {/* <div className="hero-overlay">
-        <MDBContainer className="heroHeaderContainer">
-          <MDBCol className="heroHeaderCol">
-            <MDBRow>
-              <h4 style={{color: "blue"}}>Ashers Door Services LLC</h4>
-            </MDBRow>
-            <MDBRow>
-              <h2 className="heroHeader">
-               Garage Door, Commercial Roll-up Doors & Hollow Metal Doors, Automatic Driveway Gates, Supply, Install & Repair.
-              </h2>
-            </MDBRow>
-            <MDBRow>
-              <p style={{color: "brown"}} className="heroSecondaryText heroSecondaryTextP">
-                Lorem ipsum dolor sit amet, consect orem ipsum dolor sit amet, consect orem ipsum dolor sit amet, consect orem ipsum
-                dolor sit amet, consect orem ipsum dolor sit amet, consect.
-              </p>
-            </MDBRow>
-          </MDBCol>
-          <MDBCol className="heroBtnCol">
-            <MDBBtn className="heroBtn" onClick={() => setModalShow(true)}>Learn More</MDBBtn>
-          </MDBCol>
-        </MDBContainer>
-      </div> */}
+      <div className="hero-overlay">
+        <MDBCol className="heroHeaderCol">
+          <MDBRow>
+            <h4 style={{color: "blue"}}>Ashers Door Services LLC</h4>
+          </MDBRow>
+          <MDBRow>
+            <h2 className="heroHeader">
+              <p>Supplies, Installations, & Repairs</p>
+              <p>Residential & Commercial Services</p>
+              <p>Automatic Driveway Gates.</p>
+            </h2>
+          </MDBRow>
+          {/* <MDBRow>
+            <p style={{color: "brown"}} className="heroSecondaryText heroSecondaryTextP">
+              Lorem ipsum dolor sit amet, consect orem ipsum dolor sit amet, consect orem ipsum dolor sit amet, consect orem ipsum
+              dolor sit amet, consect orem ipsum dolor sit amet, consect.
+            </p>
+          </MDBRow> */}
+        </MDBCol>
+        <MDBCol className="heroBtnCol">
+          <MDBBtn className="heroBtn" onClick={() => setModalShow(true)}>Learn More</MDBBtn>
+        </MDBCol>
+      </div>
       <AboutModal show={modalShow} onHide={() => setModalShow(false)} />
       <Carousel data-bs-theme="dark">
         <Carousel.Item>
           <img className="d-block w-100" src={img0136} alt="First slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={image0140} alt="Second slide" />
+          <img className="d-block w-100" src={image9253} alt="Second slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={image0293} alt="Third slide" />
+          <img className="d-block w-100" src={image0343} alt="Third slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={image0343} alt="Fourth slide" />
+          <img className="d-block w-100" src={image9120} alt="Fourth slide" />
         </Carousel.Item>
       </Carousel>
     </div>
